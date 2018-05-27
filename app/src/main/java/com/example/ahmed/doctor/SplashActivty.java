54c261fdf07c61c1a38d89dafc06285f0c9341fc
revel-media <ahmedfci37@yahoo.com>
2018-05-27 16:02:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ahmed/doctor/SplashActivty.java
@@ -0,0 +1,13 @@
+package com.example.ahmed.doctor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activty);
+    }
+}
